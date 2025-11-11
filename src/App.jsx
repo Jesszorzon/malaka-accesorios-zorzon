938994363048d062e3d4e1bf54952ce12607be9c
jesszorzon <jesicazorzon@gmail.com>
2025-11-11 16:31:16 -0300

    Entrega 2: Navega las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-
-import './App.css'
-import NavBar from './components/NavBar.jsx'
-import ItemListContainer from './components/ItemListContainer.jsx'
-import Hero from './components/Hero.jsx'
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Hero from "./components/hero";
 
 function App() {
   return (
     <>
       <NavBar />
       <Hero />
-      <ItemListContainer greeting="¡Bienvenidos a Malaka Accesorios! 💍" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,25 @@
-
 import CartWidget from './CartWidget.jsx'
-import logo from '../assets/logo.png'
 
 function NavBar() {
   return (
     <nav className="navbar">
-      <img src={logo} alt="Malaka Accesorios" className="logo" />
-      <ul className="nav-links">
+      <div className="navbar-logo">
+        <img src="/img/logo.png" alt="Malaka Accesorios" />
+        <h1>Malaka Accesorios</h1>
+      </div>
+
+      <ul className="navbar-links">
         <li><a href="#">Inicio</a></li>
         <li><a href="#">Productos</a></li>
         <li><a href="#">Nosotros</a></li>
         <li><a href="#">Contacto</a></li>
       </ul>
+
       <CartWidget />
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
+
+
